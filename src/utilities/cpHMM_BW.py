20612c2f2a79735670442d5060d77627c8ed45e7
nlammers371 <nlammers@berkeley.edu>
2017-06-04 17:41:35 -0700

    Sigh

--- src/utilities/cpHMM_BW.py
+++ src/utilities/cpHMM_BW.py
@@ -1,7 +1,7 @@
 import time
 import sys
 import scipy # various algorithms
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 import numpy as np
 from scipy.misc import logsumexp
 import math
